67ca0115cbb812391f4f2edc4e4d1a1f2661d954
Rizwan2456 <181400085@gift.edu.pk>
2022-02-08 16:17:55 +0500

    version 2.0

--- todo.js
+++ todo.js
@@ -1,61 +1,68 @@
-var btnAdd= document.getElementById('add');
-var btup= document.getElementById('up');
-btnAdd.onclick=()=>{
-    var input= document.getElementById('input').value;
-    var list= document.getElementById('list');
+var btnAdd = document.getElementById('add');
+var btup = document.getElementById('up');
+//add
+btnAdd.onclick = () => {
+    var input = document.getElementById('input').value;
+    var list = document.getElementById('list');
 
-    if(input==''){
+    if (input == '') {
         alert('Enter value please');
     }
-    else{
-        var li= document.createElement('li');
+    else {
+        var li = document.createElement('li');
         // li.innerHTML=input;
-        var text= document.createTextNode(input);
+        var text = document.createTextNode(input);
         li.appendChild(text);
 
-        var btn= document.createElement('button');
-        btn.id='del';
-        var t= document.createTextNode("Delete"); 
+        //create delete and update button
+        var btn = document.createElement('button');
+        btn.id = 'del';
+        var t = document.createTextNode("Delete");
         btn.appendChild(t);
         li.appendChild(btn);
 
-        var btnupdate= document.createElement('button');
-        btnupdate.id='update';
-        var t1= document.createTextNode("update"); 
+        var btnupdate = document.createElement('button');
+        btnupdate.id = 'update';
+        var t1 = document.createTextNode("update");
         btnupdate.appendChild(t1);
+
+        //append buttons with li
         li.appendChild(btnupdate);
         list.appendChild(li);
-  document.getElementById('input').value="";
+
+        document.getElementById('input').value = "";
 
     }
 
-    
-btn.onclick= function (event) {
-   var li2= event.target.parentNode;
-   console.log(li2);
-   var list2=li2.parentNode;
-   console.log(list2);
-   list2.removeChild(li2);
-}
-    
-btnupdate.onclick= function(event){
-    var li2= event.target.parentNode;
-   var text=   li2.firstChild;
-   console.log(text);
+    //delete
+    btn.onclick = function (event) {
+        var li2 = event.target.parentNode;
+        console.log(li2);
+        var list2 = li2.parentNode;
+        console.log(list2);
+        list2.removeChild(li2);
+    }
 
-  document.getElementById('input').value=text.textContent;
+    //update
+    btnupdate.onclick = function (event) {
+        var li2 = event.target.parentNode;
+        var text = li2.firstChild;
+        console.log(text);
 
-  btnAdd.style.display="none";
-    btup.style.display="inline";
-    btup.addEventListener('click', function(){
-       text.textContent= document.getElementById('input').value;
-       
-  btnAdd.style.display="inline";
-  btup.style.display="none";
-  document.getElementById('input').value="";
-    });
+        document.getElementById('input').value = text.textContent;
 
-}
+        btnAdd.style.display = "none";
+        btup.style.display = "inline";
+
+        btup.addEventListener('click', function () {
+
+            text.textContent = document.getElementById('input').value;
+            btnAdd.style.display = "inline";
+            btup.style.display = "none";
+            document.getElementById('input').value = "";
+        });
+
+    }
 
 }
 
